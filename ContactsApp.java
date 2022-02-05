06770fa9e9ef896f979ebec4fc33eac5e47febe1
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-02-05 14:38:12 -0700

    Add addContents method

--- ContactsApp.java
+++ ContactsApp.java
@@ -1,4 +1,3 @@
-/*
  * Author: Braylen Strain
  * Date:
  * 
@@ -7,9 +6,12 @@
 import java.util.*;
 
 public class ContactsApp {
+	
+	private static final Scanner USERINPUT = new Scanner(System.in);
+	final static int NUMBER_OF_DATA_FIELDS = 6;
 
 	public static void main(String[] args) {
-		
+	
 		ArrayList<Contact> contacts = new ArrayList<>();
 		
 		//Check for file name as command line argument to upload contacts
@@ -19,13 +21,11 @@ public class ContactsApp {
 			//else say no file and offer to handwrite file or manually input contacts
 		}
 		
-		Scanner userInput = new Scanner(System.in);
-		
 		//If no command line argument file was input, or input file did not exist, get file name from user or user creates new contacts list
 		while (contacts.isEmpty()) {
 			System.out.println("Would you like to input a file name to input contacts, or create a new contacts list?");
 			System.out.print("Type \"File\" or \"New\": ");
-			String answer = userInput.next();
+			String answer = USERINPUT.nextLine();
 			System.out.println();
 			
 			//If user inputs File, execute file upload method.
@@ -36,20 +36,73 @@ public class ContactsApp {
 				System.out.println("Execute file upload method");
 				
 			} else if (answer.equalsIgnoreCase("New")){
-				//TODO
-				System.out.println("Execute create new contacts method(Same as add contacts)");
-				
+				addContacts(contacts);
+			
 			} else {
-				System.out.println("Invalid input.\n");
+				System.out.println("Invalid input.");
 			}
 		}
 		//Send user to homepage
 		//TODO
 		System.out.println("Execute homepage method/write out homepage in main method");
-		userInput.close();
-	}
-	
-	public static void addContacts(ArrayList<Contact> contacts) {
 		
 	}
+	
+	//Add contacts to ArrayList contacts
+	public static void addContacts(ArrayList<Contact> contacts) {
+		int newContactsAmount = -1;
+		
+		//Get number of contacts user would like to create
+		while (newContactsAmount == -1) {
+			try {
+				System.out.print("How many contacts would you like to add?: ");
+				newContactsAmount = USERINPUT.nextInt();
+				System.out.println();
+				USERINPUT.nextLine();
+				
+			} catch (InputMismatchException ex) {
+				System.out.println("\nInvalid input, must be an integer.");
+				USERINPUT.nextLine();
+			}
+		}
+		
+		//User inputs information for each data field in each Contact object
+		System.out.println("Type in the information for each contact, or press enter to skip.\n");
+		for (int i = 0; i < newContactsAmount; i++) {
+			Contact contact = new Contact();
+			String[] contactInfo = new String[NUMBER_OF_DATA_FIELDS];
+			Set[] command = {new SetFirstName(), new SetLastName(), new SetPersonalPhone(), new SetWorkPhone(), new SetPersonalEmail(), new SetWorkEmail()};
+			
+			//Store user input in contactInfo array
+			System.out.println("Contact #" + (i + 1));
+			System.out.print("First Name: ");
+			contactInfo[0] = USERINPUT.nextLine();
+			System.out.print("Last Name: ");
+			contactInfo[1] = USERINPUT.nextLine();
+			System.out.print("Personal Phone Number: ");
+			contactInfo[2] = USERINPUT.nextLine();
+			System.out.print("Work Phone Number: ");
+			contactInfo[3] = USERINPUT.nextLine();
+			System.out.print("Personal E-mail: ");
+			contactInfo[4] = USERINPUT.nextLine();
+			System.out.print("Work E-mail: ");
+			contactInfo[5] = USERINPUT.nextLine();
+			System.out.println();
+			
+			//Import data from contactInfo array into contact data fields using the different Set... classes that implement the Set interface
+			for (int j = 0; j < NUMBER_OF_DATA_FIELDS; j++) {
+				if (contactInfo[j].trim().length() != 0) {
+					command[j].set(contactInfo[j].trim(), contact);
+				}
+			}
+			
+			//Store contact in contacts
+			contacts.add(contact);
+		}
+	}
+	
+	//for loop to print out contact info
+//	for (Contact contact: contacts) {
+//		System.out.println(contact);
+//	}
 }
